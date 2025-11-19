18746771ba5bdf6d45b97492fa756bdffb17805c
Andika Hermanto <andika.kristi@gmail.com>
2025-11-19 15:13:55 +0800

    Added sample scenes meta files

--- Tools/sync-samples.sh
+++ Tools/sync-samples.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Synchronizing Samples"
 
-rsync -a --delete -P -h --exclude='*.meta' "./Assets/PackageSamples/" "./Packages/com.spacecow.ecs.anim2d/Samples~/"
+rsync -a --delete -P -h "./Assets/PackageSamples/" "./Packages/com.spacecow.ecs.anim2d/Samples~/"
