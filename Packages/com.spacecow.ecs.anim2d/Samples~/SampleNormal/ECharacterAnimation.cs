e0089ee371af8ebeb96b19559a01614ae9062bbf
Andika Hermanto <andika.kristi@gmail.com>
2025-11-19 15:57:58 +0800

    Refactor repo to a submodule

--- Packages/com.spacecow.ecs.anim2d/Samples~/SampleNormal/ECharacterAnimation.cs
+++
@@ -1,6 +0,0 @@
-public enum ECharacterAnimation
-{
-    Idle,
-    Run,
-    Damaged,
-}
--- Tools/sync-samples.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Synchronizing Samples"
-
-rsync -a --delete -P -h "./Assets/PackageSamples/" "./Packages/com.spacecow.ecs.anim2d/Samples~/"
